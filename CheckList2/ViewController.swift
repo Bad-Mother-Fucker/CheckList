c6f921d1886836e4ab68e3167d90d94f2a6e18fd
Michele De Sena <mic.desena@studenti.unina.it>
2019-02-13 15:46:32 +0100

    Initial Commit

---
+++ CheckList2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CheckList2
+//
+//  Created by Michele De Sena on 13/02/2019.
+//  Copyright © 2019 Michele De Sena. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
